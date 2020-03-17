e126bf8f6c43412fbd6c60eb2ad00def1a1cdf0f
Michael Lynch <michael@michaelrlynch.com>
2020-03-17 03:34:19 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  PIRATE = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATE << self
+  end
+
+  def self.all
+    PIRATE
+  end
+
 end
